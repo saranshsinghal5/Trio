5d6580f7895a7fa0c1c8c26add129e40602e6b7f
saranshsinghal5 <singhalsaransh05@gmail.com>
2025-03-20 18:38:03 +0530

    hotel responsive

--- src/Components/Hotel/Hotel.jsx
+++ src/Components/Hotel/Hotel.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import AboveSelector from "./AboveSelecote";
 import Location from "./Location";
 import CheckIn from "./CheckIn";
 import CheckOut from "./CheckOut";
@@ -8,31 +7,41 @@ import SearchBtn from "../Header/SearchBtn";
 import EEE from "../Flight/EEE";
 import HotelCard from "./HotelCard";
 
-
 function Hotel() {
   return (
-    <div className="h-auto w-300 mx-10">
-      <div className="bg-white h-auto py-3 px-18 w-300 mt-[-24.5em] z-[-4] pt-15 rounded-3xl ">
-        <AboveSelector />
-        <div className=" py-2 pb-8 flex flex-row">
-            <Location/>
-            <CheckIn/>
-            <CheckOut/>
-            <PricePerNight/>
+    <div className="h-auto w-full mx-auto px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-16">
+      {/* Hotel Search Form */}
+      <div className="bg-white h-auto py-6 px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-16 w-full mt-[-24.5em] md:mt-[1em] lg:mt-[-1em] xl:mt-[-2em] 2xl:mt-[-3em] z-[-4] pt-15 rounded-3xl shadow-lg">
+        {/* <AboveSelector /> */}
+        <div className="py-4 flex flex-col sm:flex-row flex-wrap gap-4">
+          <div className="flex-1 min-w-[200px]">
+            <Location />
+          </div>
+          <div className="flex-1 min-w-[200px]">
+            <CheckIn />
+          </div>
+          <div className="flex-1 min-w-[200px]">
+            <CheckOut />
+          </div>
+          <div className="flex-1 min-w-[200px]">
+            <PricePerNight />
+          </div>
         </div>
 
-        <div >
-            <SearchBtn/>
-          </div>  
+        <div className="flex justify-center mt-4">
+          <SearchBtn />
+        </div>
       </div>
-      <div className="mt-6 mb-25">
-        <EEE/>
-       </div  >
 
-       <div className="mt-6 mb-25">
-        <HotelCard/>
-        
-       </div>
+      {/* EEE Section */}
+      <div className="mt-10 mb-10 sm:mb-15 md:mb-20 lg:mb-25 xl:mb-30 2xl:mb-35">
+        <EEE />
+      </div>
+
+      {/* Hotel Cards Section */}
+      <div className="mt-5 mb-10 sm:mb-15 md:mb-20 lg:mb-25 xl:mb-30 2xl:mb-35">
+        <HotelCard />
+      </div>
     </div>
   );
 }
